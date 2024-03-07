47d4016704464b34c1e7d7030e2d995ed056cd2f
scott davis <scott@ultralabs.io>
2024-03-07 08:23:36 -0800

    added clean dev cognito user pool script

---
+++ clean_dev_cognito_user_pool.sh
@@ -0,0 +1,22 @@
+# Set your User Pool ID
+USER_POOL_ID="us-west-2_GE4DcWMin"
+
+aws cognito-idp list-users --user-pool-id $USER_POOL_ID > user_pool.json
+
+# undesireable list
+cat user_pool.json | jq -r  '.Users[] | {Username, Email: (.Attributes[] | select(.Name == "email").Value)} | select((.Email | test(".*@ultralabs.io$") or test(".*@ourpetpolicy.com$") | not)) | .Email' > affected_emails.txt
+
+
+# disable and delete undesireable list
+cat user_pool.json |  jq -r '.Users[] | {Username, Email: (.Attributes[] | select(.Name == "email").Value)} | select((.Email | test(".*@ultralabs.io$") or test(".*@ourpetpolicy.com$") | not)) | .Username' | while read username; do
+  echo "Disabling and Deleting user $username"
+  aws cognito-idp admin-disable-user --user-pool-id $USER_POOL_ID --username "$username"
+  aws cognito-idp admin-delete-user --user-pool-id $USER_POOL_ID --username "$username"
+done
+
+# enable ultralabs.io and ourpetpolicy.com users
+cat user_pool.json | jq -r  '.Users[] | {Username, Email: (.Attributes[] | select(.Name == "email").Value)} | select((.Email | test(".*@ultralabs.io$") or test(".*@ourpetpolicy.com$"))) | .Email' > desired_emails.txt
+cat user_pool.json |  jq -r '.Users[] | {Username, Email: (.Attributes[] | select(.Name == "email").Value)} | select((.Email | test(".*@ultralabs.io$") or test(".*@ourpetpolicy.com$"))) | .Username' | while read username; do
+  echo "Enabling user $username"
+  aws cognito-idp admin-enable-user --user-pool-id $USER_POOL_ID --username "$username"
+done
