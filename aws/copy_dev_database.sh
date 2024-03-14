847c1a1d1e2249ce7d20b1d82e08e020037cccac
scott davis <scott@ultralabs.io>
2024-03-14 12:21:31 -0700

    fixed errors

--- aws/copy_dev_database.sh
+++ aws/copy_dev_database.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 SOURCE_DB_HOST="add-new-mexico-state-policy-b7ucs6kb-db.c1xjnpy2aknr.us-west-2.rds.amazonaws.com"
 SOURCE_DB_PORT="5432"
@@ -19,28 +18,23 @@ echo "downloading database: $SOURCE_DB_HOST"
 
 # Export data from the source database
 export PGPASSWORD=$SOURCE_DB_PASSWORD
-# # pg_dump -h $SOURCE_DB_HOST -p $SOURCE_DB_PORT -U $SOURCE_DB_USER -F p -d $SOURCE_DB_NAME > "$DUMP_FILE"
-pg_dump -h $SOURCE_DB_HOST -p $SOURCE_DB_PORT -U $SOURCE_DB_USER -F c -d $SOURCE_DB_NAME -f "$DUMP_FILE"
+pg_dump -h $SOURCE_DB_HOST -p $SOURCE_DB_PORT -U $SOURCE_DB_USER -F p -d $SOURCE_DB_NAME > "$DUMP_FILE"
 
-# echo "yo"
 
 if [ $? -eq 0 ]; then
     echo "uploading database: $TARGET_DB_HOST"
 
     # Prepare to import data: Disable FK constraints on the target database
     export PGPASSWORD=$TARGET_DB_PASSWORD
-    # psql -h $TARGET_DB_HOST -p $TARGET_DB_PORT -U $TARGET_DB_USER -d $TARGET_DB_NAME -c "SET session_replication_role = 'replica';"
-    # psql -h $TARGET_DB_HOST -p $TARGET_DB_PORT -U $TARGET_DB_USER -d $TARGET_DB_NAME -f "$DUMP_FILE"
-    # pg_restore: Disable FK constraints and use single transaction for atomicity
-    # pg_restore -h $TARGET_DB_HOST -p $TARGET_DB_PORT -U $TARGET_DB_USER -d $TARGET_DB_NAME --no-owner --role=$TARGET_DB_USER --single-transaction -v "$DUMP_FILE"
-    pg_restore --clean  --if-exists  -h $TARGET_DB_HOST -p $TARGET_DB_PORT -U $TARGET_DB_USER -d $TARGET_DB_NAME --no-owner --role=$TARGET_DB_USER --single-transaction -v "$DUMP_FILE"
+    psql -h $TARGET_DB_HOST -p $TARGET_DB_PORT -U $TARGET_DB_USER -d $TARGET_DB_NAME -c "SET session_replication_role = 'replica';"
+    psql -h "$TARGET_DB_HOST" -p "$TARGET_DB_PORT" -U "$TARGET_DB_USER" -d "$TARGET_DB_NAME" -v ON_ERROR_STOP=0 -f "$DUMP_FILE"
 
     if [ $? -eq 0 ]; then
         echo "Data import successful!"
     else
         echo "Data import failed."
     fi
-    # psql -h $TARGET_DB_HOST -p $TARGET_DB_PORT -U $TARGET_DB_USER -d $TARGET_DB_NAME -c "SET session_replication_role = 'origin';"
+    psql -h $TARGET_DB_HOST -p $TARGET_DB_PORT -U $TARGET_DB_USER -d $TARGET_DB_NAME -c "SET session_replication_role = 'origin';"
 
 else
     echo "Data export failed."
@@ -48,4 +42,4 @@ fi
 
 # Cleanup
 unset PGPASSWORD
-# rm -f "$DUMP_FILE"
+rm -f "$DUMP_FILE"
